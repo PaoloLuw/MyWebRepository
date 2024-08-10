c4dc7c2d82696e55b24df5f30c60d9a556004ba1
PaoloLuw <114841110+PaoloLuw@users.noreply.github.com>
2024-08-10 10:15:06 -0500

    Corrigiendo la ruta /api

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir el archivo HTML
-app.use(express.static(path.join(__dirname)));
-
 // Ejemplo de una ruta de API
 app.get('/api', (req, res) => {
   res.json({ message: '¡Hola desde la API!' });
 });
+
+// Servir el archivo HTML
+app.use(express.static(path.join(__dirname)));
 
 // Iniciar el servidor
 app.listen(PORT, () => {
